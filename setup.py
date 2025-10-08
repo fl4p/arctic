a65b6a6f7ef46ad886798cf9c4776388f27e5754
Fabian <fabian@schlieper.email>
2025-10-08 09:09:19 +0200

    v1.83.6

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ extensions = [
 
 setup(
     name="arctic",
-    version="1.83.5",
+    version="1.83.6",
     author="Man AHL Technology",
     author_email="arctic@man.com",
     description=("AHL Research Versioned TimeSeries and Tick store"),
