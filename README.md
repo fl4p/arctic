c359a82f717d5289afae23d1a6404998eb03fc27
Fabian <fabian@schlieper.email>
2025-10-09 21:49:38 +0200

    add LnQ20VQLgz

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is a fork of the old [Arctic](README-arctic.md) time series store with mong
 It comes with a couple of compression techniques to lower disk usage.
 
 * Pandas 2.x compatibility
+* Optimized `TickStore.write(df)` with pandas DataFrame is 10x-20x faster using full vectorization
 * `float32` support (use: `ts.write(..., to_dtype=np.float32)`) for reduced storage size to 60~80%)
 * Added lossy codec for floating point numbers to further improve compression. Expect 30~40% compression ratio with a max relative
   tolerance of 0.02 % (200ppm) for numbers in the range `[1e-9, 2e31]` (use: `ts.write(..., codec='loq16_10_dzv1')`)
--- arctic/tickstore/tickstore.py
+++ arctic/tickstore/tickstore.py
@@ -36,7 +36,8 @@ from .._util import indent
 try:
     from lz4.block import compress as lz4_compress, decompress as lz4_decompress
 
-    lz4_compressHC = lambda _str: lz4_compress(_str, mode='high_compression')
+    def lz4_compressHC(_str): # ordinary function so name appears in profile
+        return lz4_compress(_str, mode='high_compression')
 except ImportError as e:
     from lz4 import compress as lz4_compress, compressHC as lz4_compressHC, decompress as lz4_decompress
 
@@ -121,12 +122,15 @@ def register_codec(name, obj):
     assert len(name) < 16
     codec_registry[name] = obj
 
-register_codec('LnQ25VQLgz', LnQ16_VQL(loq_loss=25, comp='zlib')) # for l2 data, general purpose [1e-13, 2e+27]
+LnQ25VQLgz = LnQ16_VQL(loq_loss=25, comp='zlib')
+register_codec('LnQ25VQLgz',LnQ25VQLgz) # for l2 data, general purpose [1e-13, 2e+27]
 register_codec('LnQ15VQLlz4', LnQ16_VQL(loq_loss=15, comp='lz4')) # for price data, fast
 register_codec('LnQ15gz', LnQ16_zlib(loq_loss=15)) # for signed trade qty, no VQL (no auto-corr)
 # TODO like LnQ25VQLgz but smaller prescale to trade small number precision for bigger range (up to 2e35 or so)
 # this one: LnQ16_VQL(loq_loss=25, comp='zlib', log_prescale=24, loq_preadd=1e-8)
 
+LnQ20VQLgz = LnQ16_VQL(loq_loss=20, comp='zlib', log_prescale=20, loq_preadd=1e-12)
+register_codec('LnQ20VQLgz', LnQ20VQLgz) # for l2 data, general purpose [1e-15, 2e+32]
 
 def index_to_ns(series, dtype):
     try:
