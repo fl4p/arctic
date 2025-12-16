dea0844d0a01905c06e848000ace7c05a84d2dc5
Fabian <fabian@schlieper.email>
2025-12-16 06:45:25 +0000

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ extensions = [
 
 setup(
     name="arctic",
-    version="1.84.3",
+    version="1.84.4",
     author="Man AHL Technology",
     author_email="arctic@man.com",
     description=("AHL Research Versioned TimeSeries and Tick store"),
